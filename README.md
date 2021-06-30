18b38aabc0855b9665481a38ea082f681734986c
Burak Kısacık <burakkisacik@Burak-MacBook-Air.local>
2021-06-30 14:50:13 +0300

    add README

---
+++ README.md
@@ -0,0 +1,38 @@
+<h1 align="center">
+    Sprite Animation Demo
+</h1>
+
+<p align="center">
+  <img src="assets/sprite.gif" alt="animated" />
+</p>
+
+> Frame Sequence
+
+---
+
+## Description
+
+This project attempts to create sprite animation. This technique is based on showing different frames very fast so human eye can see the animation as whole. This technique also can be applied to create 2D games on browsers.
+
+---
+
+## Technologies
+
+-   Vanilla JavaScript
+-   ViteJS ( Build Tool )
+
+---
+
+## How to use
+
+```Bash
+git clone https://github.com/burakkisacik/Digital-Human-Sprite-Animation.git
+```
+
+```Bash
+npm install
+```
+
+```Bash
+npm run dev
+```
--- main.js
+++ main.js
@@ -14,60 +14,60 @@ images.character.src = './assets/jsont01.png';
 const characterActions = ['1row'];
 
 class Character {
-  constructor() {
-    this.width = 152;
-    this.height = 227;
-    this.frameX = 0;
-    this.frameY = 0;
-    this.x = canvas.width / 2;
-    this.y = canvas.height / 2 - 150;
-    this.speed = 1;
-    this.action = characterActions[0];
-    if (this.action === '1row') {
-      this.frameY = 6;
-      this.minFrame = 0;
-      this.maxFrame = 11;
+    constructor() {
+        this.width = 152;
+        this.height = 227;
+        this.frameX = 0;
+        this.frameY = 0;
+        this.x = canvas.width / 2;
+        this.y = canvas.height / 2 - 150;
+        this.speed = 1;
+        this.action = characterActions[0];
+        if (this.action === '1row') {
+            this.frameY = 6;
+            this.minFrame = 0;
+            this.maxFrame = 11;
+        }
     }
-  }
 
-  draw() {
-    drawSprite(
-      images.character,
-      this.width * this.frameX,
-      this.height * this.frameY,
-      this.width,
-      this.height,
-      this.x,
-      this.y,
-      this.width,
-      this.height
-    );
+    draw() {
+        drawSprite(
+            images.character,
+            this.width * this.frameX,
+            this.height * this.frameY,
+            this.width,
+            this.height,
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        );
 
-    if (this.frameX < this.maxFrame) {
-      this.frameX++;
-    } else {
-      this.frameX = this.minFrame;
+        if (this.frameX < this.maxFrame) {
+            this.frameX++;
+        } else {
+            this.frameX = this.minFrame;
+        }
     }
-  }
 }
 
 const girl = new Character();
 
 function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
-  ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
+    ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
 }
 
 function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  console.log('run');
+    console.log('run');
 
-  girl.draw();
+    girl.draw();
 }
 
 window.onload = setInterval(animate, 1000 / 10);
 
 window.addEventListener('resize', () => {
-  canvas.height = window.innerHeight;
-  canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
 });
